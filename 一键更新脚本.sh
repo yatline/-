2cf4d67b01e9a63c4819521e4188120af112ec0b
闫塘林 <2367475462@qq.com>
2025-09-15 22:31:06 +0800

    update on 2025-09-15 22:31:06

---
+++ 一键更新脚本.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 一键更新脚本：提交并推送到 GitHub
+
+cd /home/i/kaoyan || exit 1
+
+# 获取当前时间作为 commit 信息
+commit_msg="update on $(date '+%Y-%m-%d %H:%M:%S')"
+
+git add .
+git commit -m "$commit_msg"
+git push origin main
+
